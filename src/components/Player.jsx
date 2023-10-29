eadad40f4a35bbf2715281d5dcb53db22d56f9d1
Jorge Calvo G <jorgeecg10@gmail.com>
2023-10-29 16:54:44 -0500

    Edition part 1

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
+
 function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((prevIsEditing) => !prevIsEditing);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+  let btnCaption = "Edit";
+
+  if (isEditing) {
+    playerName = <input type="text" required value={name} />;
+    btnCaption = "Save";
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{btnCaption}</button>
     </li>
   );
 }
